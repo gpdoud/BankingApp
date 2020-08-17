5f917df03f3a315953e0e613155f96b0276ed6d5
Greg Doud <gpdoud@gmail.com>
2020-08-17 16:13:40 -0400

    Final commit

---
+++ BankingLib/Account.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp {
+    public class Account {
+
+        private static int NextAccountNumber = 1;
+        public int AccountNumber { get; private set; }
+        private double Balance { get; set; } = 0;
+        public string Description { get; set; }
+
+        private static Account[] AccountArray = new Account[5];
+        private static int NextIndex = 0;
+
+        public void Print() {
+            Console.WriteLine($"Nbr: {AccountNumber}, Desc: {Description}, Bal: {GetBalance()}");
+        }
+
+        public static void AddAccount(Account AccountInstance) {
+            AccountArray[NextIndex] = AccountInstance;
+            NextIndex++;
+        }
+
+        public static void ListAccounts() {
+            for(var idx = 0; idx < NextIndex; idx++) {
+                var account = AccountArray[idx];
+                Console.WriteLine($"Id:{account.AccountNumber}; Desc:{account.Description}; Bal:{account.GetBalance()}");
+            }
+        }
+
+        public bool Transfer(Account ToAccount, double Amount) {
+            var success = Withdraw(Amount);
+            if(!success) {
+                Console.WriteLine("Transfer failed - See log file");
+                return false;
+            }
+            success = ToAccount.Deposit(Amount);
+            if(!success) {
+                Console.WriteLine("Transfer failed - See log file");
+                Deposit(Amount);
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsAmountNegative(double Amount) {
+            if(Amount <= 0) {
+                Console.WriteLine("Amount must be positive");
+                return true;
+            }
+            return false;
+        }
+
+        public bool Deposit(double Amount) {
+            if(IsAmountNegative(Amount)) {
+                return false; ;
+            }
+            Balance += Amount;
+            return true;
+        }
+
+        public bool Withdraw(double Amount) {
+            if(IsAmountNegative(Amount)) {
+                return false;
+            }
+            try {
+                InsufficientFunds(Amount);
+            } catch (Exceptions.InsufficientFundsException ex) {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+
+            Balance -= Amount;
+            return true;
+        }
+
+        private bool InsufficientFunds(double Amount) {
+            if(Amount > Balance) {
+                var msg = $"Balance is {Balance}; Withdraw amount is {Amount}";
+                var ex = new Exceptions.InsufficientFundsException(msg);
+                ex.AccountNumber = AccountNumber;
+                ex.AccountDescription = Description;
+                ex.AccountBalance = Balance;
+                ex.WithdrawAmount = Amount;
+                throw ex;
+            }
+            return false;
+        }
+
+        public double GetBalance() {
+            return Balance;
+        }
+        public static string GetRoutingNumber() {
+            return "123 456 789";
+        }
+
+        public Account() {
+            this.AccountNumber = NextAccountNumber++;
+            this.Description = "New Account";
+        }
+    }
+}
---
+++ BankingLib/Exceptions/InsufficientFundsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp.Exceptions {
+    
+    public class InsufficientFundsException : Exception {
+
+        public int AccountNumber { get; set; }
+        public string AccountDescription { get; set; }
+        public double  AccountBalance { get; set; }
+        public double  WithdrawAmount { get; set; }
+
+        public InsufficientFundsException() 
+            : base() { }
+
+        public InsufficientFundsException(string message) 
+            : base(message) { }
+
+        public InsufficientFundsException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
